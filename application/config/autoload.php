0c3241b03974bd880aff33ef6fc442db47b67316
raflinaufal <raflinaufalalief204@gmail.com>
2024-04-03 20:30:19 +0700

    add form validation

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,8 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-// $autoload['libraries'] = array();
-
+$autoload['libraries'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -90,10 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-// $autoload['helper'] = array();
 $autoload['helper'] = array('url');
-
-
 
 /*
 | -------------------------------------------------------------------
